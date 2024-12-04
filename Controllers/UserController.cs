16b3575ea7f41c31ae6c512b0cf3ca5d811b1996
iliyan1787 <148617241+iliyan1787@users.noreply.github.com>
2024-12-04 17:10:29 +0200

    Update UserController.cs

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using EventManagamentSystem.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
