2d52727130b28f4b77abaabe3f872e62fbe83124
iliyan1787 <148617241+iliyan1787@users.noreply.github.com>
2024-12-04 17:15:15 +0200

    Update User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -1,27 +1,30 @@
-﻿using EventManagamentSystem.Models;
+using EventManagamentSystem.Models;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-public class User
+namespace EventManagamentSystem.Models
 {
-    [Key]
-    public int UserId { get; set; }
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
 
-    [Required]
-    [MaxLength(50)]
-    public string Username { get; set; } = null!;
+        [Required]
+        [MaxLength(50)]
+        public string Username { get; set; } = null!;
 
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; } = null!;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
 
-    [Required]
-    public string PasswordHash { get; set; } = null!;
+        [Required]
+        public string PasswordHash { get; set; } = null!;
 
-    [Required]
-    public string Role { get; set; } = null!; // User or Admin 
+        [Required]
+        public string Role { get; set; } = null!; // User or Admin 
 
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
-    public ICollection<Attendee> Attendees { get; set; } = new List<Attendee>();
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public ICollection<Attendee> Attendees { get; set; } = new List<Attendee>();
 
+    }
 }
